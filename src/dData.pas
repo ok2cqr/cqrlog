2269cf0552d1aab7e0aa4df99b614f45330d576c
Petr Hlozek <petr@ok2cqr.com>
2014-06-22 07:54:46 +0200

    table call_alet added

--- src/dData.pas
+++ src/dData.pas
@@ -24,7 +24,7 @@ uses
 const
   MaxCall   = 100000;
   cDB_LIMIT = 500;
-  cDB_MAIN_VER = 9;
+  cDB_MAIN_VER = 10;
   cDB_COMN_VER = 3;
   cDB_PING_INT = 300;  //ping interval for database connection in seconds
                        //program crashed after long time of inactivity
@@ -3056,6 +3056,26 @@ begin
       if old_version < 9 then
       begin
         Q1.SQL.Text := 'alter table log_changes add upddeleted int(1) default 0';
+        if fDebugLevel>=1 then Writeln(Q1.SQL.Text);
+        Q1.ExecSQL
+      end;
+
+      if old_version < 10 then
+      begin
+        Q1.SQL.Add('CREATE TABLE call_alert (');
+        Q1.SQL.Add('  id int NOT NULL AUTO_INCREMENT PRIMARY KEY,');
+        Q1.SQL.Add('  callsign varchar(20) NOT NULL,');
+        Q1.SQL.Add('  band varchar(6) NULL,');
+        Q1.SQL.Add('  mode varchar(6) NULL');
+        Q1.SQL.Add(') COLLATE '+QuotedStr('utf8_bin')+';');
+        if fDebugLevel>=1 then Writeln(Q1.SQL.Text);
+        Q1.ExecSQL;
+
+        Q1.SQL.Text := 'ALTER TABLE call_alert ADD INDEX (id);';
+        if fDebugLevel>=1 then Writeln(Q1.SQL.Text);
+        Q1.ExecSQL;
+
+        Q1.SQL.Text := 'ALTER TABLE call_alert ADD INDEX (callsign);';
         if fDebugLevel>=1 then Writeln(Q1.SQL.Text);
         Q1.ExecSQL
       end;
