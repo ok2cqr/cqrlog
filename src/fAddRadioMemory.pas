2e7c8e15e9bd1bee0b261f74071cb5879053e167
Petr Hlozek <petr@ok2cqr.com>
2016-01-27 20:50:43 +0100

    USB mode were lost when memory was edited

--- src/fAddRadioMemory.pas
+++ src/fAddRadioMemory.pas
@@ -22,6 +22,7 @@ type
     Label2: TLabel;
     Label3: TLabel;
     procedure Button1Click(Sender: TObject);
+    procedure FormCreate(Sender : TObject);
     procedure FormShow(Sender: TObject);
   private
     { private declarations }
@@ -41,11 +42,6 @@ uses dUtils;
 procedure TfrmAddRadioMemory.FormShow(Sender: TObject);
 begin
   dmUtils.LoadFontSettings(frmAddRadioMemory);
-
-  dmUtils.InsertModes(cmbMode);
-  cmbMode.Items.Delete(cmbMode.Items.IndexOf('SSB'));
-  cmbMode.Items.Insert(1,'USB');
-  cmbMode.Items.Insert(2,'LSB');
 
   edtFreq.SetFocus
 end;
@@ -69,6 +65,14 @@ begin
   end;
 
   ModalResult := mrOK
+end;
+
+procedure TfrmAddRadioMemory.FormCreate(Sender : TObject);
+begin
+  dmUtils.InsertModes(cmbMode);
+  cmbMode.Items.Delete(cmbMode.Items.IndexOf('SSB'));
+  cmbMode.Items.Insert(1,'USB');
+  cmbMode.Items.Insert(2,'LSB')
 end;
 
 initialization
