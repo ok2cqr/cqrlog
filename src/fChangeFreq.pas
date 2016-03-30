3e9e0f1dc2a9ed703947574d7c467af7acaac3a3
Petr Hlozek <petr@petrhlozek.cz>
2016-03-30 19:25:40 +0200

    added MHz description

--- src/fChangeFreq.pas
+++ src/fChangeFreq.pas
@@ -23,12 +23,19 @@ type
     edtTXOffset : TEdit;
     edtSSB: TEdit;
     Label1: TLabel;
+    Label10 : TLabel;
+    Label11 : TLabel;
+    Label12 : TLabel;
+    Label13 : TLabel;
+    Label14 : TLabel;
     Label2: TLabel;
     Label3: TLabel;
     Label4: TLabel;
     Label5: TLabel;
     Label6 : TLabel;
     Label7 : TLabel;
+    Label8 : TLabel;
+    Label9 : TLabel;
     procedure btnOKClick(Sender: TObject);
   private
     { private declarations }
