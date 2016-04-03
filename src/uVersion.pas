8a6b4a5f582744e7d9bb3b9eb9233f1095259fff
Petr Hlozek <petr@petrhlozek.cz>
2016-04-03 07:42:43 +0200

    build number increased

--- src/uVersion.pas
+++ src/uVersion.pas
@@ -4,12 +4,12 @@ unit uVersion;
 interface
 
 const
-  cVERSION    = '1.9.1 (003)';
+  cVERSION    = '1.9.1 (005)';
   cMAJOR      = 1;
   cMINOR      = 9;
   cRELEAS     = 1;
-  cBUILD      = 3;
-  cBUILD_DATE = '2015-03-06';
+  cBUILD      = 5;
+  cBUILD_DATE = '2015-04-03';
 
 implementation
 
