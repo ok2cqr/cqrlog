09f346f77d8594df77504fd0a32462f7d0aaec22
ok2cqr <petr@ok2cqr.com>
2016-05-07 07:23:37 +0200

    version changed to 2.0.1

--- src/uVersion.pas
+++ src/uVersion.pas
@@ -4,12 +4,12 @@ unit uVersion;
 interface
 
 const
-  cVERSION    = '2.0.0 (002)';
+  cVERSION    = '2.0.1 (001)';
   cMAJOR      = 2;
   cMINOR      = 0;
-  cRELEAS     = 0;
-  cBUILD      = 2;
-  cBUILD_DATE = '2016-04-21';
+  cRELEAS     = 1;
+  cBUILD      = 0;
+  cBUILD_DATE = '2016-05-07';
 
 implementation
 
