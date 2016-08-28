8786708cbe3c96e508714864194debaeb25d07e5
ok2cqr <petr@ok2cqr.com>
2016-08-28 18:59:27 +0200

    version changed to 2.0.2

--- src/uVersion.pas
+++ src/uVersion.pas
@@ -4,12 +4,12 @@ unit uVersion;
 interface
 
 const
-  cVERSION    = '2.0.1 (001)';
+  cVERSION    = '2.0.2 (001)';
   cMAJOR      = 2;
   cMINOR      = 0;
-  cRELEAS     = 1;
+  cRELEAS     = 2;
   cBUILD      = 0;
-  cBUILD_DATE = '2016-05-07';
+  cBUILD_DATE = '2016-08-28';
 
 implementation
 
