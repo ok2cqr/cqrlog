db165de5a83c340264571be5e4fadb75e60a0102
ok2cqr <petr@ok2cqr.com>
2017-03-05 06:25:19 +0100

    added more paths for library search

--- src/dData.pas
+++ src/dData.pas
@@ -4356,6 +4356,7 @@ begin
     Paths.Add('/lib/x86_64-linux-gnu/');
 
     Paths.Add('/usr/lib/i386-linux-gnu/');
+    Paths.Add('/lib/i386-linux-gnu/');
     Paths.Add('/usr/lib/');
     Paths.Add('/lib/');
     Paths.Add('/usr/lib/mysql/');
