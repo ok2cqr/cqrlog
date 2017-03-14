ee78e24851bf2781076514d0e19a3b3c514faf90
OH1KH <oh1kh@sral.fi>
2017-03-14 11:59:30 +0200

    Trying to merge to 2.0.5

---
+++ src/uVersion.pas
@@ -0,0 +1,17 @@
+unit uVersion;
+
+{$mode objfpc}
+interface
+
+const
+  cVERSION    = '2.0.5 (019)';
+  cMAJOR      = 2;
+  cMINOR      = 0;
+  cRELEAS     = 4;
+  cBUILD      = 0;
+  cBUILD_DATE = '2017-03-12';
+
+implementation
+
+end.
+
