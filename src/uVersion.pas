47b2945a5dbe3d56bed50a3133ef819c57f7e7c5
OH1KH <oh1kh@sral.fi>
2017-03-14 11:53:37 +0200

    Trying to merge to 2.0.5

--- src/uVersion.pas
+++ src/uVersion.pas
@@ -4,12 +4,12 @@ unit uVersion;
 interface
 
 const
-  cVERSION    = '2.0.4 (019)';
+  cVERSION    = '2.0.5 (010)';
   cMAJOR      = 2;
   cMINOR      = 0;
-  cRELEAS     = 4;
+  cRELEAS     = 5;
   cBUILD      = 0;
-  cBUILD_DATE = '2017-03-10';
+  cBUILD_DATE = '2017-03-12';
 
 implementation
 
