98a5491c8902918312421cc3a71d3be884d97c11
Petr Hlozek <petr@ok2cqr.com>
2017-04-20 08:18:06 +0200

    callsign change from OK2CQR to OK7AN

--- src/fExportProgress.pas
+++ src/fExportProgress.pas
@@ -611,7 +611,7 @@ begin
   AssignFile(f, FileName);
   Rewrite(f);
   Writeln(f, 'ADIF export from CQRLOG for Linux version '+dmData.VersionString);
-  Writeln(f, 'Copyright (C) ',YearOf(now),' by Petr, OK2CQR and Martin, OK1RR');
+  Writeln(f, 'Copyright (C) ',YearOf(now),' by Petr, OK7AN and Martin, OK1RR');
   Writeln(f);
   Writeln(f, 'Internet: http://www.cqrlog.com');
   Writeln(f);
--- src/fLoTWExport.pas
+++ src/fLoTWExport.pas
@@ -343,7 +343,7 @@ begin
   date := FormatDateTime('yyyy-mm-dd',now);
   Writeln(f, '<ADIF_VER:5>2.2.1');
   Writeln(f, 'ADIF export from CQRLOG for Linux version '+dmData.VersionString);
-  Writeln(f, 'Copyright (C) ',YearOf(now),' by Petr, OK2CQR and Martin, OK1RR');
+  Writeln(f, 'Copyright (C) ',YearOf(now),' by Petr, OK7AN and Martin, OK1RR');
   Writeln(f);
   Writeln(f, 'Internet: http://www.cqrlog.com');
   Writeln(f);
