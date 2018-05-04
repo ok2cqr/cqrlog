458938b9a71ac78cbb3341e57c7f83babeff12cd
OH1KH <oh1kh@sral.fi>
2018-05-04 10:19:21 +0300

    eQSL up&dnload: preferences button did not open right tab

--- src/feQSLDownload.pas
+++ src/feQSLDownload.pas
@@ -232,9 +232,9 @@ end;
 
 procedure TfrmeQSLDownload.btnPreferencesClick(Sender : TObject);
 begin
+  cqrini.WriteInteger('Pref', 'ActPageIdx', 18);  //set lotw tab active. Number may change if preferences page change
   with TfrmPreferences.Create(self) do
   try
-    pgPreferences.ActivePage := tabLoTW;
     ShowModal
   finally
     Free
