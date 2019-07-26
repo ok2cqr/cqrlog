56837af7c100c16d62b6ed84fe2465c50d306ce7
Petr Hlozek <petr@petrhlozek.cz>
2019-07-26 21:00:27 +0200

    build date updated

--- src/uVersion.pas
+++ src/uVersion.pas
@@ -4,12 +4,12 @@ unit uVersion;
 interface
 
 const
-  cVERSION    = '2.3.0 (002)';
+  cVERSION    = '2.3.0 (003)';
   cMAJOR      = 2;
   cMINOR      = 3;
   cRELEAS     = 0;
-  cBUILD      = 2;
-  cBUILD_DATE = '2018-12-03';
+  cBUILD      = 3;
+  cBUILD_DATE = '2019-07-26';
 
 implementation
 
