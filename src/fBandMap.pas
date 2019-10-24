3678b7c53d859f1afd8c1b40fd215f2470759e49
phl0 <florian@florian-wolters.de>
2019-10-24 15:57:53 +0200

    Include own location into xplanet display

--- src/fBandMap.pas
+++ src/fBandMap.pas
@@ -746,6 +746,9 @@ var
   UseDefaultColor : Boolean;
   DefaultColor    : Integer;
   MaxXplanetSpots : Integer;
+  myloc: string = '';
+  mycall: string = '';
+  lat, long: currency;
 begin
   UseDefaultColor := cqrini.ReadBool('xplanet','UseDefColor',True);
   DefaultColor    := cqrini.ReadInteger('xplanet','color',clWhite);
@@ -754,6 +757,15 @@ begin
   DeleteFile(FxplanetFile);
 
   l := TStringList.Create;
+  myloc := cqrini.ReadString('Station', 'LOC', '');
+  mycall := cqrini.ReadString('Station', 'Call', '');
+  xColor := IntToHex(DefaultColor,8);
+  xColor := '0x'+Copy(xColor,3,Length(xColor)-2);
+  if dmUtils.IsLocOK(myloc) then
+  begin
+     dmUtils.CoordinateFromLocator(dmUtils.CompleteLoc(myloc), lat, long);
+     l.Add(CurrToStr(lat)+' '+CurrToStr(long)+' "'+mycall+'" color='+xColor);
+  end;
   try
     for i:=1 to MAX_ITEMS do
     begin
