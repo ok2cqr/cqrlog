efe8153d46d3fa395b6ce46aec44c72ea1270c58
OH1KH <oh1kh@sral.fi>
2020-01-16 20:14:38 +0200

    merged latest changes from official master

--- src/uVersion.pas
+++ src/uVersion.pas
@@ -4,12 +4,12 @@ unit uVersion;
 interface
 
 const
-  cVERSION    = '2.4.0 (105)';
+  cVERSION    = '2.4.0 (106)';
   cMAJOR      = 2;
   cMINOR      = 4;
   cRELEAS     = 0;
   cBUILD      = 1;
-  cBUILD_DATE = '2019-12-30';
+  cBUILD_DATE = '2020-01-16';
 
 implementation
 
