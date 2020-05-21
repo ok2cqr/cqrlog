ff1514757f047246ba64deead8a5814b27abdcf4
OH1KH <oh1kh@sral.fi>
2020-05-21 17:56:10 +0300

    version nr

--- src/uVersion.pas
+++ src/uVersion.pas
@@ -4,12 +4,12 @@ unit uVersion;
 interface
 
 const
-  cVERSION    = '2.4.0 (118)';
+  cVERSION    = '2.4.0 (119)';
   cMAJOR      = 2;
   cMINOR      = 4;
   cRELEAS     = 0;
   cBUILD      = 1;
-  cBUILD_DATE = '2020-04-20';
+  cBUILD_DATE = '2020-04-21';
 
 implementation
 
