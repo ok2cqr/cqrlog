1f22f0c455d2e88287aa93d917d2aa79dad8f7a5
Petr Hlozek <petr@petrhlozek.cz>
2020-09-06 13:57:48 +0200

    InternalConnection class and database utils added
    Usfull for one-time methods eg. modify something,
    getting some value from a table etc.

---
+++ src/uDbUtils.pas
@@ -0,0 +1,54 @@
+(*
+ ***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License.        *
+ *                                                                         *
+ ***************************************************************************
+*)
+
+unit uDbUtils;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, strutils, uInternalConnection;
+
+  (*
+  * Returns connection to CQRLOG database. Usfull for one-time methods eg. modify something
+  * getting some value from a table etc.
+  *)
+  function  GetNewInternalConnection(): TInternalConnection;
+
+  procedure UpdateConnectionInfoRecord(Host, Port, UserName, Password : String; DatabaseName : String = '');
+  procedure UpdateConnectionInfoDatabaseName(DatabaseNumber : Integer);
+
+implementation
+
+var
+  ConnectionInfo : TConnectionInfo;
+
+procedure UpdateConnectionInfoRecord(Host, Port, UserName, Password : String; DatabaseName : String = '');
+begin
+  ConnectionInfo.HostName := Host;
+  ConnectionInfo.Port := Port;
+  ConnectionInfo.UserName := UserName;
+  ConnectionInfo.Password := Password;
+  ConnectionInfo.DatabaseName := DatabaseName;
+end;
+
+procedure UpdateConnectionInfoDatabaseName(DatabaseNumber : Integer);
+begin
+  ConnectionInfo.DatabaseName := 'cqrlog' + AddChar('0',IntToStr(DatabaseNumber), 3);
+end;
+
+function GetNewInternalConnection(): TInternalConnection;
+begin
+  Result := TinternalConnection.Create(ConnectionInfo);
+end;
+
+end.
+
---
+++ src/uInternalConnection.pas
@@ -0,0 +1,115 @@
+(*
+ ***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License.        *
+ *                                                                         *
+ ***************************************************************************
+*)
+
+unit uInternalConnection;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, sqldb, mysql57dyn, mysql57conn;
+
+type
+  TConnectionInfo = record
+    HostName: string;
+    Port: String;
+    UserName: String;
+    Password: String;
+    DatabaseName: String;
+  end;
+
+type
+  TinternalConnection = class
+  private
+    _ConnectionInfo : TConnectionInfo;
+    Connection : TConnectionName;
+    TC : TSQLTransaction;
+
+    function  getNewMySQLConnectionObject() : TConnectionName;
+    function  getNewMySQLConnection: TConnectionName;
+
+    procedure init();
+  public
+    Q : TSQLQuery;
+    T : TSQLTransaction;
+
+    constructor Create(ConnectionInfo : TConnectionInfo);
+    destructor  Destroy; override;
+  end;
+
+implementation
+
+constructor TInternalConnection.Create(ConnectionInfo : TConnectionInfo);
+begin
+  _ConnectionInfo := ConnectionInfo;
+  Q := TSQLQuery.Create(nil);
+  T := TSQLTransaction.Create(nil);
+  TC := TSQLTransaction.Create(nil);
+
+  init();
+end;
+
+procedure TinternalConnection.init();
+begin
+  Connection := getNewMySQLConnection();
+  T.DataBase := Connection;
+  Q.DataBase := Connection;
+  Q.Transaction := T;
+  T.Action := caNone;
+end;
+
+function TInternalConnection.getNewMySQLConnectionObject() : TConnectionName;
+var
+  Conn : TMySQL57Connection;
+begin
+  Conn := TMySQL57Connection.Create(nil);
+  Conn.SkipLibraryVersionCheck := True;
+  Conn.KeepConnection := True;
+
+  Result := Conn;
+end;
+
+function TInternalConnection.getNewMySQLConnection : TConnectionName;
+var
+  Conn : TConnectionName;
+begin
+  Conn := getNewMySQLConnectionObject();
+  Conn.Transaction := TC;
+  Conn.HostName := _ConnectionInfo.HostName;
+  Conn.Params.Text  := 'Port=' + _ConnectionInfo.Port;
+  Conn.UserName := _ConnectionInfo.UserName;
+  Conn.Password := _ConnectionInfo.Password;
+  Conn.DatabaseName := 'information_schema';
+  Conn.Connected := true;
+
+  Conn.ExecuteDirect(
+    'SET SESSION sql_mode=(SELECT REPLACE(@@sql_mode,'+QuotedStr('ONLY_FULL_GROUP_BY')+','+QuotedStr('')+'));'
+  );
+
+  Conn.ExecuteDirect('use ' + _ConnectionInfo.DatabaseName);
+
+  Result := Conn;
+end;
+
+destructor TInternalConnection.Destroy;
+begin
+  inherited;
+
+  Q.Close;
+  Connection.Close(True);
+  FreeAndNil(Q);
+  FreeAndNil(T);
+  FreeAndNil(Connection);
+  FreeAndNil(TC);
+end;
+
+end.
+
