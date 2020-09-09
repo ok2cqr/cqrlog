498fada9f0618d35e4ab77157f3481983c8cbab2
OH1KH <oh1kh@sral.fi>
2020-09-09 17:52:12 +0300

    Added Ignore QSL feature to Labelprint and Automark QSL

--- src/fExLabelPrint.pas
+++ src/fExLabelPrint.pas
@@ -62,7 +62,7 @@ var
 implementation
 {$R *.lfm}
 
-uses dUtils, dData, uMyIni, fQSLExpPref, dDXCC,fMain;
+uses dUtils, dData, uMyIni, fQSLExpPref, dDXCC,fMain, dLOgUpload;
 { TfrmExLabelPrint }
 
 procedure TfrmExLabelPrint.edtQSOsToLabelExit(Sender: TObject);
@@ -427,6 +427,9 @@ begin
       exit
   end;
 
+  if cqrini.ReadBool('OnlineLog','IgnoreQSL',False) then
+           dmLogUpload.DisableOnlineLogSupport;
+
   FieldCount := GetExpFieldCount;
   if dmData.DebugLevel >= 1 then Writeln('Field count: ', FieldCount);
   dmData.CreateQSLTmpTable;
@@ -511,7 +514,10 @@ begin
     dmData.DropQSLTmpTable;
     lblProgress.Caption := 'Complete!';
     CloseFile(f);
-    dmData.RefreshMainDatabase()
+    dmData.RefreshMainDatabase();
+
+    if cqrini.ReadBool('OnlineLog','IgnoreQSL',False) then
+     dmLogUpload.EnableOnlineLogSupport;
   end
 end;
 
