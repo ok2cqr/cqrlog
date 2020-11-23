8520e1cb152bc9663f6bca954505100a348f428e
OH1KH <oh1kh@sral.fi>
2020-11-23 18:41:43 +0200

    version info

--- src/uVersion.pas
+++ src/uVersion.pas
@@ -4,18 +4,12 @@ unit uVersion;
 interface
 
 const
-  cVERSION    = '2.4.0 (128)';
+  cVERSION    = '2.4.0 (129)';
   cMAJOR      = 2;
   cMINOR      = 4;
   cRELEAS     = 0;
   cBUILD      = 1;
-<<<<<<< HEAD
-  cBUILD_DATE = '2020-11-18';
-||||||| d6bbec1
-  cBUILD_DATE = '2020-10-27';
-=======
-  cBUILD_DATE = '2019-10-28';
->>>>>>> 7ce656ec10ce70f83acd09d38ab908ebaeb2e804
+  cBUILD_DATE = '2020-11-23';
 
 implementation
 
