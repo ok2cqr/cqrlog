86687d38bf17166d64b8ade270fb121d469cd961
OH1KH <oh1kh@sral.fi>
2021-03-18 10:44:01 +0200

    Version 2.5.2(105)

--- src/uVersion.pas
+++ src/uVersion.pas
@@ -4,12 +4,12 @@ unit uVersion;
 interface
 
 const
-  cVERSION    = '2.5.2 (104)';
+  cVERSION    = '2.5.2 (105)';
   cMAJOR      = 2;
   cMINOR      = 5;
   cRELEAS     = 2;
   cBUILD      = 1;
-  cBUILD_DATE = '2021-03-06';
+  cBUILD_DATE = '2021-03-18';
 
 implementation
 
