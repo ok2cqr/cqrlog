517c9bcbca9a76acbf3dac42deba7c185fe6b43f
OH1KH <oh1kh@sral.fi>
2021-11-16 09:02:17 +0200

    start vers(114)

--- src/uVersion.pas
+++ src/uVersion.pas
@@ -4,13 +4,13 @@ unit uVersion;
 interface
 
 const
-  cVERSION    = '2.5.2 (113)';
+  cVERSION    = '2.5.2 (114)';
   cMAJOR      = 2;
   cMINOR      = 5;
   cRELEAS     = 2;
   cBUILD      = 1;
 
-  cBUILD_DATE = '2021-11-13';
+  cBUILD_DATE = '2021-11-16';
 
 implementation
 
